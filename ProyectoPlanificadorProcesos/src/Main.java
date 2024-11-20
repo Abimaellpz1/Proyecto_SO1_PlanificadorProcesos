b34c21f4dda42408eb5c7a2e97b1b5f726b05407
Abimael López Armenta <160942522+Abimaellpz1@users.noreply.github.com>
2024-11-20 10:13:33 -0700

    Delete ProyectoPlanificadorProcesos directory

--- ProyectoPlanificadorProcesos/src/Main.java
+++
@@ -1,7 +0,0 @@
-package src;
-public class Main {
-    public static void main(String[] args) {
-        SimuladorPlanificacion simulador = new SimuladorPlanificacion();
-        simulador.iniciarSimulacion();
-    }
-}
